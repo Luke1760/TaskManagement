d6db234c6b2ff7bac3062e219bb94f62aaf27c51
Luke1760 <kjd1760@gmail.com>
2022-10-30 12:31:53 +0800

    add: task #index & views/tasks/index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
 class TasksController < ApplicationController
+
+  def index
+    @query = current_user.tasks.ransack(params[:query])
+    @tasks = @query.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+    end
+  end
 end
