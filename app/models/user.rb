06cb66761bc331811d60894969007623c86e9990
Luke1760 <kjd1760@gmail.com>
2022-10-30 11:12:54 +0800

    add: model User validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # has_secure_password 會自動附帶3個驗證：
   #檢查密碼是否存在, 密碼長度必須是不是大於 72 bytes, 確認密碼填寫是否正確 (using a XXX_confirmation attribute)
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
