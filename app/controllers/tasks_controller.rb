905389b2d93da3aaecbe5c0431751798b54b6ffa
Luke1760 <kjd1760@gmail.com>
2022-10-30 12:34:25 +0800

    add: task #new、#create & views/tasks/new、_form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,25 @@ class TasksController < ApplicationController
       format.html
     end
   end
+
+  def new 
+    @task = Task.new
+  end
+
+  def create 
+    @task = current_user.tasks.new(set_task_params)
+
+    if @task.save 
+      Rails.logger.info "tasks: #{@task.attributes.inspect}"
+      redirect_to task_path(@task), notice: "已新增任務「#{@task.name}」。"
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def set_task_params
+    params.require(:task).permit(:name, :description)
+  end
 end
