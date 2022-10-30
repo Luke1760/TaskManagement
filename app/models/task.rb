99f6c5f65ba2be8f1117b2afacc3b462794e38a4
Luke1760 <kjd1760@gmail.com>
2022-10-30 14:37:00 +0800

    add: Task order sort

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   belongs_to :user
   validate  :set_nameless_name
   validates :name, presence: true, length: { maximum: 30 }
+  
+  default_scope { order(created_at: :desc) }
 
   private 
 
